e0d4333690c814f31e5a5b31106cec2df7382802
mohamed mhiri <mohamad.elmhiri@gmail.com>
2023-06-02 18:02:45 +0100

    feat: add tailwindcss + custom favicon

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: "/favicon-32x32.png" }]
+    }
+  }
 })
