6d5f3b3b93032a5e32c37b89703e4952a69a37b6
mohamed mhiri <mohamad.elmhiri@gmail.com>
2023-06-10 10:21:50 +0100

    feat: comment the components

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="flex flex-col w-screen hidden-overflow">
+    <!-- Hero section -->
     <div class="flex-auto bg-desktop"></div>
+    <!-- Main box -->
     <div class="grow hidden-overflow main-background">
       <div class="flex flex-col items-center justify-center main-content lg:gap-y-8">
+        <!-- Top section -->
         <div class="basis-1/12 lg:w-4/12 xl:w-4/12 sm:w-10/12 md:w-10/12">
           <div class="h-100 max-w-screen-xl flex-row flex">
+            <!-- Title section -->
             <h1 class="text-5xl text-h1 bold-font-family text-white grow tracking-widest w-full">
               TODO
             </h1>
+            <!-- Theme section -->
             <button type="button basis-1/6" @click="useDarkMode"
               v-if="colorMode.value == 'light' || colorMode.value == 'system'">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="white"
@@ -25,18 +30,20 @@
             </button>
           </div>
         </div>
+        <!-- Form section -->
         <div class="basis-1/12 lg:w-4/12 xl:w-4/12 sm:w-10/12 md:w-10/12">
           <div class="max-w-screen-xl">
-            <div class="h-100 w-full flex items-center p-3 container input-box rounded"
-              @keyup="displayCurrentlyTypingSpan" @mousedown="displayCurrentlyTypingSpan"
-              @mouseleave="hideCurrentlyTypingSpan">
+            <div class="h-100 w-full flex items-center p-3 container input-box rounded">
+              <!-- Add Todo checkbox -->
               <label class="checkbox-block" for="newTodoState" @mouseover="showWhiteMark()" @mouseleave="hideWhiteMark()">
                 <input class="todo-checkbox" type="checkbox" id="newTodoState" :checked="newTodo.isCompleted"
                   @change="toggleNewTodoItemState()" />
                 <span class="checkmark" ref="checkMark"></span>
                 <span class="hide-white-mark" ref="whiteMark"></span>
               </label>
-              <div class="flex items-center grow">
+              <!-- Add Todd Input -->
+              <div class="flex items-center grow" @keyup="displayCurrentlyTypingSpan"
+                @mousedown="displayCurrentlyTypingSpan" @mouseleave="hideCurrentlyTypingSpan">
                 <span v-if="isMounted && !isMobile"
                   :class="currentlyTypingSpanIsDisplayed ? 'display-content' : 'hidden-content'"
                   class="ml-9 flex select-none items-center pl-3 text-very-dark-gray sm:text-sm bg-transparent"
@@ -57,14 +64,18 @@
             </div>
           </div>
         </div>
+        <!-- List section -->
         <div class="grow lg:w-4/12 xl:w-4/12 sm:w-10/12 md:w-10/12">
           <div class="h-100 max-w-screen-xl flex bg-teal-lightest">
             <div class="list-box rounded shadow w-full">
+              <!-- Todo List -->
               <draggable v-model="filteredTodo" tag="ul" item-key="_id" class="divide-y divide-solid todo-list"
                 @update="moveItem">
                 <template #item="{ element, index }">
+                  <!-- Todo Item -->
                   <li class="flex item-style items-center" @mouseover="showXButton(index)"
                     @mouseleave="hideXButton(index)">
+                    <!-- Item checkbox -->
                     <label class="checkbox-block" :for="'todo-item-' + index" @mouseover="showWhiteMarkByKey(index)"
                       @mouseleave="hideWhiteMarkByKey(index)">
                       <input class="todo-checkbox" type="checkbox" :id="'todo-item-' + index"
@@ -72,12 +83,14 @@
                       <span class="checkmark" :ref="el => { checkMarks[index] = el }"></span>
                       <span class="hide-white-mark" :ref="el => { whiteMarks[index] = el }"></span>
                     </label>
+                    <!-- Item label -->
                     <p @click="toggleTodoItemState(index)"
                       :class="todoItemStateVModels[index] ? 'line-through text-very-light-gray' : 'text-very-dark-gray'"
                       class="w-full ml-9 normal-font-weight normal-font-family pointer"
                       :ref="el => { todoItems[index] = el }">
                       {{ element.label }}
                     </p>
+                    <!-- Item delete button -->
                     <button type="button" class="hide-button" :ref="el => { xButtons[index] = el }"
                       @click="deleteTodo(element)">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -88,29 +101,39 @@
                   </li>
                 </template>
               </draggable>
+              <!-- Todo footer -->
               <div class="sticky items-footer p-4 flex flex-row md:items-center md:justify-center">
+                <!-- Left items (mobile) -->
                 <p v-if="isMounted && isMobile"
                   class="basis-1/4 small-font-size bold-font-weight light-font-family text-dark-gray">
                   {{ leftItems }} item{{ leftItems > 1 ? 's' : '' }} left
                 </p>
+                <!-- Left items (desktop) -->
                 <p v-if="isMounted && !isMobile"
                   class="basis-1/3 small-font-size bold-font-weight light-font-family text-dark-gray">
                   {{ leftItems }} item{{ leftItems > 1 ? 's' : '' }} left
                 </p>
+                <!-- White space (mobile) -->
                 <div v-if="isMounted && isMobile" class="grow ml-6"></div>
+                <!-- Filter buttons (desktop) -->
                 <div v-if="isMounted && !isMobile" class="basis-2/4 ml-6">
+                  <!-- All items button (desktop) -->
                   <button @mouseover="onHoverAllItemsButton" @mouseleave="onLeaveAllItemsButton"
                     @click="onClickAllItemsButton" :class="allItemsButtonColor" class="m-1">All</button>
+                  <!-- Active items button (desktop) -->
                   <button @mouseover="onHoverActiveItemsButton" @mouseleave="onLeaveActiveItemsButton"
                     @click="onClickActiveItemsButton" :class="activeItemsButtonColor" class="m-1">Active</button>
+                  <!-- Completed items button (desktop) -->
                   <button @mouseover="onHoverCompletedItemsButton" @mouseleave="onLeaveCompletedItemsButton"
                     @click="onClickCompletedItemsButton" :class="completedItemsButtonColor" class="m-1">Completed</button>
                 </div>
+                <!-- Clear completed (desktop) -->
                 <button v-if="isMounted && !isMobile" type="button" @mouseover="onHoverClearCompleted"
                   @click="onClearCompleted" @mouseleave="onLeaveClearCompleted" :class="clearCompletedTextColor"
                   class="basis-1/4 small-font-size">
                   Clear Completed
                 </button>
+                <!-- Clear completed (mobile) -->
                 <button v-if="isMounted && isMobile" type="button" @mouseover="onHoverClearCompleted"
                   @click="onClearCompleted" @mouseleave="onLeaveClearCompleted" :class="clearCompletedTextColor"
                   class="basis-1/3 small-font-size">
@@ -120,17 +143,22 @@
             </div>
           </div>
         </div>
+        <!-- Filter buttons (mobile) -->
         <div class="basis-1/12 lg:w-4/12 xl:w-4/12 sm:w-10/12 md:w-10/12">
           <div v-if="isMounted && isMobile"
             class="p-4 flex flex-row items-center justify-center sm:content-around bg-teal-lightest buttons-box w-full">
+            <!-- All items button (mobile) -->
             <button @mouseover="onHoverAllItemsButton" @mouseleave="onLeaveAllItemsButton" @click="onClickAllItemsButton"
               :class="allItemsButtonColor" class="m-1">All</button>
+            <!-- Active items button (mobile) -->
             <button @mouseover="onHoverActiveItemsButton" @mouseleave="onLeaveActiveItemsButton"
               @click="onClickActiveItemsButton" :class="activeItemsButtonColor" class="m-1">Active</button>
+            <!-- Completed items button (mobile) -->
             <button @mouseover="onHoverCompletedItemsButton" @mouseleave="onLeaveCompletedItemsButton"
               @click="onClickCompletedItemsButton" :class="completedItemsButtonColor" class="m-1">Completed</button>
           </div>
         </div>
+        <!-- Drag and drop footer -->
         <div class="basis-1/12 lg:w-4/12 xl:w-4/12 sm:w-10/12 md:w-10/12">
           <div class="h-100 w-full flex items-center justify-center">
             <p class="text-gray-500 small-font-size bold-font-weight text-light-gray">
